c9e3d18a2e2600cb676e2cba58b7b48967059f46
roab2010 <baohainao2016@gmail.com>
2025-11-24 11:09:57 +0700

    sua footer

--- trangchu.php
+++ trangchu.php
@@ -248,18 +248,18 @@ if (!empty($search_query)) {
     <div>
       <h3>VỀ CHÚNG TÔI</h3>
       <p>
-        3 chàng lính ngự lâm là thương hiệu thời trang dành cho mọi lứa tuổi,
+        3 chàng lính ngự lâm (BCB) là thương hiệu thời trang dành cho mọi lứa tuổi,
         mang đến phong cách hiện đại và năng động.
       </p>
     </div>
     <div>
       <h3>DANH MỤC</h3>
-      <p>Đồ Nam, Đồ Nữ, Đồ Trẻ Em, Khuyến Mãi</p>
+      <p>TOP, BOTTOMS, OUTWEAR, ACCESSORIES</p>
     </div>
     <div>
       <h3>THÔNG TIN LIÊN HỆ</h3>
       <p>
-        Email: support@vettins.vn<br />Hotline: 1900 1234<br />Địa chỉ: TP. Hồ
+        Email: support@vettins.vn<br />Hotline: 0397789902<br />Địa chỉ: 180 CAO LỖ PHƯỜNG 4 QUẬN 8 TP. Hồ
         Chí Minh
       </p>
     </div>
